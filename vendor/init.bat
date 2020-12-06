b03cca4901bb7ad3f081df50fdce36f453f16bb1
dgames <dgames@dtg.local>
2020-12-06 09:55:30 -0500

    revert

--- vendor/init.bat
+++ vendor/init.bat
@@ -232,9 +232,9 @@ set git_executable=
 REM echo Git User Path XXX: %CMDER_USER_GIT_PATH%
 
 :: our last hope: our own git...
-:VENDORED_GIT
 REM if exist "%CMDER_ROOT%\vendor\git-for-windows" (
 if not defined GIT_INSTALL_ROOT if defined GIT_VERSION_VENDORED (
+    :VENDORED_GIT
     set "GIT_INSTALL_ROOT=%CMDER_ROOT%\vendor\git-for-windows"
     set GIT_INSTALL_TYPE=VENDOR
     %lib_console% debug_output "Newer user Git NOT found using vendored Git '%GIT_VERSION_VENDORED%'..."
@@ -255,34 +255,35 @@ goto :CONFIGURE_GIT
 setlocal enabledelayedexpansion
 if "%GIT_INSTALL_TYPE%" equ "VENDOR" (
     set "GIT_INSTALL_ROOT=%CMDER_ROOT%\vendor\git-for-windows"
-    %lib_console% debug_output "Using Git from '!GIT_INSTALL_ROOT!..."
+    if defined GIT_VERSION_USER (
+        %lib_console% debug_output "Using Git from '!GIT_INSTALL_ROOT!..."
 
-    :: Add git to the path
-    %lib_path% enhance_path "!GIT_INSTALL_ROOT!\cmd" ""
-
-    :: Add the unix commands at the end to not shadow windows commands like more
-    if %nix_tools% equ 1 (
-        %lib_console% verbose_output "Preferring Windows commands"
-        set "path_position=append"
+        call "%cmder_root%\vendor\bin\cmder_sub_git_for_windows_path.cmd" "%CMDER_USER_GIT_PATH%" "%GIT_INSTALL_ROOT%\"
     ) else (
-        %lib_console% verbose_output "Preferring *nix commands"
-        set "path_position="
-    )
+        %lib_console% debug_output "Using Git from '!GIT_INSTALL_ROOT!..."
 
-    if %nix_tools% geq 1 (
-        if exist "!GIT_INSTALL_ROOT!\mingw32" (
-            %lib_path% enhance_path "!GIT_INSTALL_ROOT!\mingw32\bin" !path_position!
-        ) else if exist "!GIT_INSTALL_ROOT!\mingw64" (
-            %lib_path% enhance_path "!GIT_INSTALL_ROOT!\mingw64\bin" !path_position!
+        :: Add git to the path
+        %lib_path% enhance_path "!GIT_INSTALL_ROOT!\cmd" ""
+
+        :: Add the unix commands at the end to not shadow windows commands like more
+        if %nix_tools% equ 1 (
+            %lib_console% verbose_output "Preferring Windows commands"
+            set "path_position=append"
+        ) else (
+            %lib_console% verbose_output "Preferring *nix commands"
+            set "path_position="
         )
-        %lib_path% enhance_path "!GIT_INSTALL_ROOT!\usr\bin" !path_position!
+
+        if %nix_tools% geq 1 (
+            if exist "!GIT_INSTALL_ROOT!\mingw32" (
+                %lib_path% enhance_path "!GIT_INSTALL_ROOT!\mingw32\bin" !path_position!
+            ) else if exist "!GIT_INSTALL_ROOT!\mingw64" (
+                %lib_path% enhance_path "!GIT_INSTALL_ROOT!\mingw64\bin" !path_position!
+            )
+            %lib_path% enhance_path "!GIT_INSTALL_ROOT!\usr\bin" !path_position!
+        )
     )
-) else if defined GIT_VERSION_USER (
-    %lib_console% debug_output "Using Git from '!GIT_INSTALL_ROOT!..."
-
-    call "%cmder_root%\vendor\bin\cmder_sub_git_for_windows_path.cmd" "%CMDER_USER_GIT_PATH%" "%GIT_INSTALL_ROOT%\"
 )
-
 endlocal & set GIT_INSTALL_ROOT=%GIT_INSTALL_ROOT% & set path=%path%
 
 :: define SVN_SSH so we can use git svn with ssh svn repositories
