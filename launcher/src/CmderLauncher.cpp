f9e2de220b4a1609a03fd8813014915a87aa5181
Dax T. Games <dtgames@kinggeek.org>
2025-01-05 13:28:34 -0500

    x

--- launcher/src/CmderLauncher.cpp
+++ launcher/src/CmderLauncher.cpp
@@ -299,7 +299,7 @@ void StartCmder(std::wstring  path = L"", bool is_single_mode = false, std::wstr
 		// Set path to Cmder user ConEmu config file
 		PathCombine(userCfgPath, userConfigDirPath, L"user-ConEmu.xml");
 	}
-	
+
 	if (wcscmp(cpuCfgPath, L"") != 0 && (PathFileExists(cpuCfgPath) || use_user_cfg == false)) // config/[host specific terminal emulator config] file exists or /m was specified on command line, use machine specific config.
 	{
 		if (cfgRoot.length() == 0) // '/c [path]' was NOT specified
@@ -308,7 +308,8 @@ void StartCmder(std::wstring  path = L"", bool is_single_mode = false, std::wstr
 			{
 				if (!CopyFile(cfgPath, cpuCfgPath, FALSE))
 				{
-				if (PathFileExists(windowsTerminalDir)) {
+				    if (PathFileExists(windowsTerminalDir))
+					{
 						MessageBox(NULL,
 							(GetLastError() == ERROR_ACCESS_DENIED)
 							? L"Failed to copy vendor/windows-terminal/settings/settings.json file to config/windows_terminal_%COMPUTERNAME%_settings.json! Access Denied."
@@ -329,7 +330,8 @@ void StartCmder(std::wstring  path = L"", bool is_single_mode = false, std::wstr
 			{
 				if (!CopyFile(cpuCfgPath, cfgPath, FALSE))
 				{
-    				if (PathFileExists(windowsTerminalDir)) {
+    				if (PathFileExists(windowsTerminalDir))
+					{
 						MessageBox(NULL,
 							(GetLastError() == ERROR_ACCESS_DENIED)
 							? L"Failed to copy config/windows_terminal_%COMPUTERNAME%_settings.json file to vendor/windows-terminal/settings/settings.json! Access Denied."
@@ -628,7 +630,7 @@ void StartCmder(std::wstring  path = L"", bool is_single_mode = false, std::wstr
 	// Try to find m'intty.exe' so ConEmu can launch using %MINTTY_EXE% in external Git for Cmder Mini.
 	// See: https://github.com/Maximus5/ConEmu/issues/2559 for why this is commented.
 
-	/* 
+	/*
 	if (PathFileExists(vendoredGit))
 	{
 		PathCombine(minTTYPath, vendoredGit, L"usr\\bin\\mintty.exe");
@@ -666,7 +668,7 @@ void StartCmder(std::wstring  path = L"", bool is_single_mode = false, std::wstr
 	sei.lpFile = terminalPath;
 	sei.lpParameters = args;
 	sei.nShow = SW_SHOWNORMAL;
-	
+
 	if (admin && ShellExecuteEx(&sei))
 	{
 		if (!sei.hProcess)
