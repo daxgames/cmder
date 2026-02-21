3ea6067d1dc7e3a4b0a0f9cf44e1b3c46f73e502
Dax T. Games <dtgames@kinggeek.org>
2026-02-21 07:40:32 -0500

    rehome to daxgames/cmder

---
+++ scripts/rehome.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+branches=$1
+orgs=$2
+
+source_branch=$(echo ${branches} | awk -F: '{print $1}')
+dest_branch=$(echo ${branches} | awk -F: '{print $2}')
+
+source_org=$(echo ${orgs} | awk -F: '{print $1}')
+dest_org=$(echo ${orgs} | awk -F: '{print $2}')
+
+sed -i "s/${source_branch}/${dest_branch}/g;s/${source_org}/${dest_org}/g;s/, \"development\"//;s/^\s\+- development//" $(grep -R "${source_branch}\|${source_org}\|development" ./.github | awk -F: '{print $1}')
