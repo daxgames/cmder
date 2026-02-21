6d62043ff1e3167e3992bae1327e8bf43a3e2f8c
Dax T. Games <dtgames@kinggeek.org>
2026-02-21 10:22:39 -0500

    add scripts/rehome.sh

--- scripts/rehome.sh
+++ scripts/rehome.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <source_branch>:<dest_branch> <source_org>:<dest_org>"
+    exit 1
+fi
+
 branches=$1
 orgs=$2
 
@@ -9,4 +14,4 @@ dest_branch=$(echo ${branches} | awk -F: '{print $2}')
 source_org=$(echo ${orgs} | awk -F: '{print $1}')
 dest_org=$(echo ${orgs} | awk -F: '{print $2}')
 
-sed -i "s/${source_branch}/${dest_branch}/g;s/${source_org}/${dest_org}/g;s/, \"development\"//;s/^\s\+- development//" $(grep -R "${source_branch}\|${source_org}\|development" ./.github | awk -F: '{print $1}')
+sed -i "s/${source_branch}/${dest_branch}/g;s/${source_org}/${dest_org}/g;s/, \"development\"//;s/^\s\+- development//" $(grep -R "${source_branch}\|${source_org}\|development" ./.github | awk -F: '{print $1}' | grep -v workflows/branches.yml)
